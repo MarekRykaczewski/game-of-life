5fd40c4f36a4ba4e036b31ed67938bb17b904935
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-21 13:45:25 +0200

    refactor state tests

--- python/test_game_of_life.py
+++ python/test_game_of_life.py
@@ -1,10 +1,17 @@
-from game_of_life import dead_state
+from game_of_life import dead_state, random_state
 
 def test_dead_state():
-  assert dead_state(5, 5) == [
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-      [0,0,0,0,0],
-      [0,0,0,0,0]
-  ]
+    width, height = 5, 5
+    result = dead_state(width, height)
+
+    assert len(result) == width
+    assert all(len(row) == height for row in result)
+    assert all(cell == 0 for row in result for cell in row)
+
+def test_random_state():
+    width, height = 5, 5
+    result = random_state(width, height)
+
+    assert len(result) == width
+    assert all(len(row) == height for row in result)
+    assert all(cell in [0, 1] for row in result for cell in row)
