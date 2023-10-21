10b2f25860e10580aec3b7fae33a75c467eac6fa
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-21 17:47:23 +0200

    implement and test get_neighbours

--- python/game_of_life.py
+++ python/game_of_life.py
@@ -12,6 +12,15 @@ def random_state(width, height):
     
     return state
 
+def get_neighbours(pos, state):
+    rows = len(state)
+    cols = len(state[0]) if rows else 0
+
+    for i in range(max(0, pos[0] - 1), min(rows, pos[0] + 2)):
+        for j in range(max(0, pos[1] - 1), min(cols, pos[1] + 2)):
+            if (i, j) != pos:
+                yield state[i][j]
+      
 def render(state):
   for row in state:
       print(' '.join(['X' if cell else 'O' for cell in row]))
--- python/test_game_of_life.py
+++ python/test_game_of_life.py
@@ -1,4 +1,4 @@
-from game_of_life import dead_state, random_state, render
+from game_of_life import dead_state, get_neighbours, random_state, render
 
 def test_dead_state():
     width, height = 5, 5
@@ -21,6 +21,16 @@ def test_random_state():
     assert all(len(row) == height for row in result)
     # All cells in the result are either 0 or 1 (random state)
     assert all(cell in [0, 1] for row in result for cell in row)
+
+def test_neighbours():
+    state = [
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+
+    position = (0, 1)
+    assert list(get_neighbours(position, state)) == [1, 1, 1, 1, 1]
 
 def test_render(capsys):
     state = [
