01a0a7a04cd39535eb7371576d5a71aeb37c030e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-21 12:50:11 +0200

    implement and test function to return dead state

---
+++ python/game_of_life.py
@@ -0,0 +1,3 @@
+def dead_state(width, height):
+  arr = [[0]*width]*height
+  return arr
---
+++ python/test_game_of_life.py
@@ -0,0 +1,10 @@
+from game_of_life import dead_state
+
+def test_dead_state():
+    assert dead_state(5, 5) == [
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0]
+    ]
