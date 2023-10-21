168064e99e43b206e979225f4103232cc14512d5
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-10-21 13:44:57 +0200

    add function to return random_state

--- python/game_of_life.py
+++ python/game_of_life.py
@@ -1,3 +1,13 @@
+import random
+
 def dead_state(width, height):
   arr = [[0]*width]*height
   return arr
+
+def random_state(width, height):
+  state = dead_state(width, height)
+
+  for i in range(width):
+    for j in range(height):
+      state[i][j] = random.randint(0, 1)
+  return state
